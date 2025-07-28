3fb52d8f500547ec9fdabe776e9ac08340ae62ab
aisamii <aisakiseki@gmail.com>
2025-07-28 14:36:37 +0300

    Handle firstNumber and secondNumber input

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello!");
+        
+        Console.Write("Input the first number: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+        Console.Write("Input the second number: ");
+        int secondNumber = int.Parse(Console.ReadLine());
     }
 }
